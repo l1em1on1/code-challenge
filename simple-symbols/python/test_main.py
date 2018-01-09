a85158f355138803a94c876d87e0fb3ede90e9e7
Lukasz Sawicki <l1em1on1@gmail.com>
2018-01-09 15:56:49 +0000

    Update test_main.py

--- simple-symbols/python/test_main.py
+++ simple-symbols/python/test_main.py
@@ -2,7 +2,7 @@ import main
 import pytest
 
 @pytest.mark.parametrize("string, expected", [
-    ('+d+=3=+s+', True),
+    ('+d+=3=s+', True),
     ('f++d+', False),
     ('+++++++f++d+', True),
     ('+++++++f++d+++=====+a', False) 
